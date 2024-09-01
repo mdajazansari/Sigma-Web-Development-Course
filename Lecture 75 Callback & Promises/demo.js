1193a85914cf4424c13f85ef78ab7a77c415f09a
mdajazansari <mdajazansari2912@gmail.com>
2024-09-01 15:21:21 +0530

    Lecture 75 to 82
    Lecture 75 to 82

---
+++ Lecture 75 Callback & Promises/demo.js
@@ -0,0 +1,10 @@
+// let n = 5;
+// for (let i = 1; i <= 5; i++) {
+//     console.log(`${i} -`, i*i);   
+// }
+
+// your code goes here
+let a = 8;
+let b = 4;
+
+console.log(`${a+b}\n${a-b}\n${a/b}\n${a*b}`);
